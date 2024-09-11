6ba1c20ede2a6154486cdc24b41a24404fd54ac9
jmei403 <jason.mei403@gmail.com>
2024-09-11 19:22:42 -0400

    initial UI for Header and HomePage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import HomePage from './components/HomePage';
+import Header from './components/Header';
+
 function App() {
-  return <>App</>;
+  return (
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        <HomePage />
+      </section>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
