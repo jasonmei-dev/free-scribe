e6a86198aefe9747a467701a42de4755e432b11b
jmei403 <jason.mei403@gmail.com>
2024-09-12 17:20:44 -0400

    add audio recording feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
       <h1 className="font-medium">
         Free<span className="text-blue-400 bold">Scribe</span>
       </h1>
-      <button className="flex items-center gap-2 px-4 py-2 rounded-lg text-blue-400 specialBtn">
+      <button className="flex items-center gap-2 px-3 py-2 rounded-lg text-blue-400 text-sm specialBtn">
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
       </button>
