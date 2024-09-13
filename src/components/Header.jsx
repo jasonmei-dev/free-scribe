41ce7d2d328d0afdbc2f9764b80800f72c67b87b
jmei403 <jason.mei403@gmail.com>
2024-09-13 13:32:42 -0400

    add nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 const Header = () => {
   return (
     <header className="flex items-center justify-between gap-4 p-4">
-      <h1 className="font-medium">
-        Free<span className="text-blue-400 bold">Scribe</span>
-      </h1>
-      <button className="flex items-center gap-2 px-3 py-2 rounded-lg text-blue-400 text-sm specialBtn">
+      <a href="/">
+        <h1 className="font-medium">
+          Free<span className="text-blue-400 bold">Scribe</span>
+        </h1>
+      </a>
+      <a href="/" className="flex items-center gap-2 px-3 py-2 rounded-lg text-blue-400 text-sm specialBtn">
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
-      </button>
+      </a>
     </header>
   );
 };
