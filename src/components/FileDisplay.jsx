1fc117d5098501fd78887f5539dcf8285e744a77
jmei403 <jason.mei403@gmail.com>
2024-09-17 11:04:55 -0400

    render audio player on file display page

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,4 +1,22 @@
+import { useRef, useEffect } from 'react';
+
 const FileDisplay = ({ file, audioStream, handleAudioReset, handleFormSubmission }) => {
+  const audioRef = useRef();
+
+  useEffect(() => {
+    if (!file && !audioStream) {
+      return;
+    }
+
+    if (file) {
+      console.log('HERE FILE', file);
+      audioRef.current.src = URL.createObjectURL(file);
+    } else {
+      console.log('HERE AUDIO', audioStream);
+      audioRef.current.src = URL.createObjectURL(audioStream);
+    }
+  }, [audioStream, file]);
+
   return (
     <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center text-center pb-20 w-72 sm:w-96 max-w-full mx-auto">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
@@ -6,7 +24,12 @@ const FileDisplay = ({ file, audioStream, handleAudioReset, handleFormSubmission
       </h1>
       <div className="flex flex-col text-left my-4">
         <h3 className="font-semibold">Name</h3>
-        <p>{file ? file?.name : 'Custom audio'}</p>
+        <p className="truncate">{file ? file?.name : 'Custom audio'}</p>
+      </div>
+      <div className="flex flex-col mb-2">
+        <audio ref={audioRef} className="w-full" controls>
+          Your browser does not support audio element
+        </audio>
       </div>
       <div className="flex items-center justify-between gap-4">
         <button onClick={handleAudioReset} className="text-slate-400 hover:text-blue-600 duration-200">
